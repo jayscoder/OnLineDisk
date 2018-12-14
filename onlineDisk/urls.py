36696f8d6aea2edaefea05438ee9d45ba78fab97
WangTong <785271992@qq.com>
2018-12-15 01:56:40 +0800

    更改了urls bug

--- onlineDisk/urls.py
+++ onlineDisk/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from mainDisk import views
 from . import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     #path(r'mainDisk/', views.index),
 ]
 
